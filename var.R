4890b3c7ad4d60572a147c41f2043def62922b81
gandradeti <gandradeti@gmail.com>
2025-11-11 10:20:44 -0300

    variáveis - alterações

--- var.R
+++ var.R
@@ -22,18 +22,42 @@ print(calcular_valor_anual)
 # São case-sensitive -> idade, Idade e IDADE são varáveis diferentes
 
 nome <- "Gabriel"
-.idade <- 19
-fruta_favorita <- "Melancia"
-fruta favorita <- "Melancia"
-fruta-favorita <- "Melancia"
-if <- 10
+.idade <- 19 # variável oculta
+fruta_favorita <- "Melancia" 
+fruta favorita <- "Melancia" # não funciona
+fruta-favorita <- "Melancia" # não funciona, pois utiliza o símbolo de subtração.
+if <- 10 # erro, pois utiliza nome de função
 idade <- 20
 Idade <- 22
 IDADE <- 25
 
 
-
-
 # Tipos de Dados ----------------------------------------------------------
 
+# Numeric
+decimal <- 4.5
+inteiro <- 10
 
+class(decimal)
+class(inteiro)
+
+# Character
+texto <- "Tipos de Dados R"
+numero_texto <- "5"
+class(texto)
+class(numero_texto)
+
+# Logical
+logico_1 <- TRUE
+logico_2 <- FALSE
+class(logico_1)
+
+
+# DESAFIO ---------------------------------------------------------------
+
+valor_inicial <- 150
+valor_recebido <- 1000
+valor_gasto <- 200
+
+valor_total <- valor_inicial + valor_recebido - valor_gasto
+print(valor_total)  
