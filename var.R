79e510c67baaeefc1bb3e93e67a3cbc0553e0a8c
gandradeti <gandradeti@gmail.com>
2025-11-11 10:18:02 -0300

    variáveis

---
+++ var.R
@@ -0,0 +1,39 @@
+
+# Atribuição de Variáveis -------------------------------------------------
+
+x <- 10
+print(x)
+
+# Operações com Variáveis -------------------------------------------------
+
+valor_mensal <- 200
+meses_do_ano <- 12
+
+calcular_valor_anual <- valor_mensal * meses_do_ano
+print(calcular_valor_anual)
+
+# Nomeação de Variáveis --------------------------------------------------
+
+# Deve começar com uma letra ou com um ponto (.) seguido de uma letra
+# Pode conter letras, números, pontos (.) e underlines (_)
+####################################################################
+# Não pode conter espaços ou outros símbolos ($, %, &, - etc.)
+# Não pode ser uma palavra reservada (if, print, TRUE, etc)
+# São case-sensitive -> idade, Idade e IDADE são varáveis diferentes
+
+nome <- "Gabriel"
+.idade <- 19
+fruta_favorita <- "Melancia"
+fruta favorita <- "Melancia"
+fruta-favorita <- "Melancia"
+if <- 10
+idade <- 20
+Idade <- 22
+IDADE <- 25
+
+
+
+
+# Tipos de Dados ----------------------------------------------------------
+
+
